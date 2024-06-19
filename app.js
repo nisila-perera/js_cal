3f5da8d15f64e886f543a18dc7b91b769cb20e9b
Nisila Perera <nisilainsitha1@gmail.com>
2024-06-19 11:39:32 +0530

    new1

---
+++ app.js
@@ -0,0 +1,22 @@
+function calc(){
+    let num1="";
+    let num2="";
+    let operator=document.getElementById("operaters").value;
+    num1=new Number(document.getElementById("num 1").value);
+    num2=new Number(document.getElementById("num 2").value); 
+    let lblOutput=document.getElementById("output");
+    let answer;
+   
+    switch(operator){
+        case "+":answer=num1+num2;break;
+        case "-":answer=num1-num2;break;
+        case "*":answer=num1*num2;break;
+        case "/":answer=num1/num2;break;
+       
+   
+    }
+    lblOutput.innerHTML=answer;
+
+
+
+}
